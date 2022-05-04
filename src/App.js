e2f5e774e016df09fab88f55ec8fb6683ccb08d3
muzahid604 <604muzahid@gmail.com>
2022-05-04 11:03:23 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Component/Home/Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
