28839b3bdc02146bd5ea0744126b43850a1d48d1
muzahid604 <604muzahid@gmail.com>
2022-05-04 17:40:13 +0600

    add an extra section

--- src/Component/Home/Social/Social.js
+++ src/Component/Home/Social/Social.js
@@ -1,17 +1,16 @@
 import React from 'react';
-
+import { SocialIcon } from 'react-social-icons';
 const Social = () => {
     return (
         <div>
-            <section className="cintainer social pt-5">
+            <section className="social pt-5">
                 <h1 className="text-center fw-bolder mt-5 pt-5">SOCIAL NETWORKS</h1>
                 <h3 className="text-center">Get in touch with us in social sites</h3>
-                <p className="text-center">
-                    <a className="social-icons" href=""><i className="fab fa-facebook-f social-icon"></i></a>
-                    <a className="social-icons" href=""><i className="fab fa-instagram social-icon"></i></a>
-                    <a className="social-icons" href=""><i className="fab fa-twitter social-icon"></i></a>
-                    <a className="social-icons" href=""><i className="fab fa-linkedin-in social-icon"></i></a>
-
+                <p className="text-center m-5" >
+                    <SocialIcon className='p-3 m-2' url="https://www.facebook.com/" />
+                    <SocialIcon className='p-3 m-2' url="https://github.com/" />
+                    <SocialIcon className='p-3 m-2' url="https://linkedin.com/in/jaketrent" />
+                    <SocialIcon className='p-3 m-2' url='https://twitter.com/' />
                 </p>
             </section>
         </div>
